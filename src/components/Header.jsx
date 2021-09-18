346d2e8ebd5033527e3d45a5e46db5510292b56f
maxsergeev <makcim2108@gmail.com>
2021-09-18 15:17:51 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/647px-Apple_logo_black.svg.png'/>
+        </header>
+    );
+}
+
+export default Header;

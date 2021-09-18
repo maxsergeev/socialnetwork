0fd2c2378462577c612da3c59b62304dcd6b78ba
maxsergeev <makcim2108@gmail.com>
2021-09-18 16:31:50 +0300

    delete setting on menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
           <div><a>Messages</a></div>
           <div><a>News</a></div>
           <div><a>Music</a></div>
+          <div><a>Settings</a></div>
         </nav>
     );
 }

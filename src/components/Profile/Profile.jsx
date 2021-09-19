d46ba561f01e0e38be129a060dd2d176affbd242
maxsergeev <makcim2108@gmail.com>
2021-09-19 17:57:50 +0300

    New components. Add Route-content

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import style from './Profile.module.css';
 import profilePhoto from './../../images/profile.png'
 import MyPosts from './MyPosts/MyPosts';
+
 const Profile = () => {
     return (
-        <div className={style.content}>
-          <div>
-            <img src={profilePhoto}/>
-          </div>
-          <div>ava+desc</div>
-          <MyPosts />
-      </div>
+        <div>
+            <div>
+                <img src={profilePhoto}/>
+            </div>
+            <div>ava+desc</div>
+            <MyPosts/>
+        </div>
     );
 }
 

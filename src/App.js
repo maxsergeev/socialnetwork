9fbbd09413a14639668ce62fea43d7ce184818d2
maxsergeev <makcim2108@gmail.com>
2021-09-19 18:59:04 +0300

    del Route Music

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const App = () => {
                     <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/profile' component={Profile}/>
                     <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>
                 </div>
             </div>
